1aaeeaa32b305b01c04382f1c71728b8c2d05a36
Maria Carpenter <163090551+MECarpenter@users.noreply.github.com>
2026-02-21 22:05:31 -0800

    reservation form
    starting reservation form steps

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+
+/* ================= CAROUSEL SECTION ================= */
+
 document.addEventListener("DOMContentLoaded", function () {
 
   let currentSlide = 0;
@@ -22,4 +25,39 @@ document.addEventListener("DOMContentLoaded", function () {
     showSlide(currentSlide);
   };
 
+});
+
+// ================= Reservation Page =================
+
+const nextBtn = document.getElementById("nextBtn");
+const backBtn = document.getElementById("backBtn");
+
+const step1 = document.getElementById("step1");
+const step2 = document.getElementById("step2");
+
+nextBtn.addEventListener("click", () => {
+
+  const party = document.getElementById("partySizeSelect").value;
+  const date = document.getElementById("dateSelect").value;
+  const time = document.getElementById("timeSelect").value;
+  const occasion = document.getElementById("occasionSelect").value;
+
+  if (!party || !date || !time) {
+    alert("Please complete all required fields.");
+    return;
+  }
+
+  // Fill hidden Netlify fields
+  document.getElementById("finalPartySize").value = party;
+  document.getElementById("finalDate").value = date;
+  document.getElementById("finalTime").value = time;
+  document.getElementById("finalOccasion").value = occasion;
+
+  step1.classList.remove("active");
+  step2.classList.add("active");
+});
+
+backBtn.addEventListener("click", () => {
+  step2.classList.remove("active");
+  step1.classList.add("active");
 });
